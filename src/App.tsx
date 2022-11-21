0a445dd0db14845fa9257055ec6d1c33d691b7c2
Dezmou <incredibledez@gmail.com>
2022-11-21 19:40:08 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -6,45 +6,15 @@ function App() {
   useEffect(() => {
     ; (async () => {
       async function testWebGPU() {
-        const adapter = await navigator.gpu.requestAdapter();
-        if (!adapter) {
-          return;
-        }
+        const adapter = (await navigator.gpu.requestAdapter())!;
         const device = await adapter.requestDevice();
-        console.log(device ? 'WebGPU works' : ':(');
-
-        writeToBufferFromJS(device);
-        // await writeCopyAndReadBuffer(device);
+        console.log(device);
         await matrixMultiplication(device);
-      }
-
-      function writeToBufferFromJS(device : GPUDevice) {
-        // Get a GPU buffer in a mapped state and an arrayBuffer for writing.
-        const gpuBuffer = device.createBuffer({
-          // map at creation
-          // so that it is owned by the CPU
-          // and accessible to read/write from JS
-          // For a GPU to be able to access a buffer
-          // it needs to be unmapped: gpuBuffer.unmap()
-          // The concept of map/unmapped is needed
-          // to prevent race conditions when GPU and CPU
-          // access memory at the same time.
-          mappedAtCreation: true,
-          size: 4,
-          usage: GPUBufferUsage.MAP_WRITE,
-        });
-        // Returns an ArrayBuffer with the raw binary data
-        const arrayBuffer = gpuBuffer.getMappedRange();
-
-        // Write bytes to buffer
-        // using a TypedArray
-        const array = new Uint8Array(arrayBuffer);
-        array.set([0, 1, 2, 3]);
       }
 
       async function matrixMultiplication(device: GPUDevice) {
         // Result Matrix
-        const resultMatrixBufferSize = Float32Array.BYTES_PER_ELEMENT * 64
+        const resultMatrixBufferSize = Float32Array.BYTES_PER_ELEMENT *( 256 * 8 * 8 + 1)
         const resultMatrixBuffer = device.createBuffer({
           size: resultMatrixBufferSize,
           usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC,
@@ -78,9 +48,9 @@ function App() {
           code: /* wgsl */`
             @group(0) @binding(0) var<storage, read_write> resultMatrix : array<f32>;
       
-            @compute @workgroup_size(8, 8)
+            @compute @workgroup_size(256)
             fn main(@builtin(global_invocation_id) global_id : vec3<u32>) {
-              resultMatrix[0] = 14;
+              resultMatrix[global_id.z * 8 + global_id.y * 8 + global_id.x] = 69;
             }
           `,
         });
@@ -95,14 +65,11 @@ function App() {
           },
         });
 
-
         const commandEncoder = device.createCommandEncoder();
         const passEncoder = commandEncoder.beginComputePass();
         passEncoder.setPipeline(computePipeline);
         passEncoder.setBindGroup(0, bindGroup);
-        // In GPU compute encoding a comand to execute a kernel function on a set
-        // of data is called dispatching.
-        passEncoder.dispatchWorkgroups(1, 1);
+        passEncoder.dispatchWorkgroups(64);
         passEncoder.end();
 
         const gpuReadBuffer = device.createBuffer({
@@ -118,18 +85,22 @@ function App() {
           resultMatrixBufferSize /* size */
         );
 
-        // Submit GPU commands.
         const gpuCommands = commandEncoder.finish();
         device.queue.submit([gpuCommands]);
 
-        // Read buffer.
         await gpuReadBuffer.mapAsync(GPUMapMode.READ);
         const arrayBuffer = gpuReadBuffer.getMappedRange();
-        console.log('matrix: ', new Float32Array(arrayBuffer));
+        const res = new Float32Array(arrayBuffer);
+        let total = 0;
+        for (let i=0; i< res.length;i++){
+          if (res[i] === 69){
+            total += 1;
+          }
+        }
+        console.log(`${total} / ${res.length}`);
       }
 
       testWebGPU();
-
     })()
   }, [])
   return <>
