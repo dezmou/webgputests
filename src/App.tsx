9152d5f17a81b6d093091ddf64ed28ace53b3e31
Dezmou <incredibledez@gmail.com>
2023-01-17 16:03:24 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -77,7 +77,7 @@ function App() {
             info : u32,
           };
 
-          @group(0) @binding(0) var<storage, read> data : array<u32>;
+          @group(0) @binding(0) var<storage, read> text1 : array<u32>;
           @group(0) @binding(1) var<storage, read_write> result : array<u32>;
 
           const SHA256_BLOCK_SIZE = 32;
@@ -103,14 +103,14 @@ function App() {
             // for ( ; i < 64; ++i)
             //   m[i] = SIG1(m[i - 2]) + m[i - 7] + SIG0(m[i - 15]) + m[i - 16];
 
-            // a = ctx->state[0];
-            // b = ctx->state[1];
-            // c = ctx->state[2];
-            // d = ctx->state[3];
-            // e = ctx->state[4];
-            // f = ctx->state[5];
-            // g = ctx->state[6];
-            // h = ctx->state[7];
+            // a = (*ctx).state[0];
+            // b = (*ctx).state[1];
+            // c = (*ctx).state[2];
+            // d = (*ctx).state[3];
+            // e = (*ctx).state[4];
+            // f = (*ctx).state[5];
+            // g = (*ctx).state[6];
+            // h = (*ctx).state[7];
 
             // for (i = 0; i < 64; ++i) {
             //   t1 = h + EP1(e) + CH(e,f,g) + k[i] + m[i];
@@ -125,21 +125,21 @@ function App() {
             //   a = t1 + t2;
             // }
 
-            // ctx->state[0] += a;
-            // ctx->state[1] += b;
-            // ctx->state[2] += c;
-            // ctx->state[3] += d;
-            // ctx->state[4] += e;
-            // ctx->state[5] += f;
-            // ctx->state[6] += g;
-            // ctx->state[7] += h;
+            // (*ctx).state[0] += a;
+            // (*ctx).state[1] += b;
+            // (*ctx).state[2] += c;
+            // (*ctx).state[3] += d;
+            // (*ctx).state[4] += e;
+            // (*ctx).state[5] += f;
+            // (*ctx).state[6] += g;
+            // (*ctx).state[7] += h;
           }
 
 
           fn sha256_update(ctx : ptr<function, SHA256_CTX> , len : u32)
           {
             for (var i: u32 = 0; i < len; i++) {
-              (*ctx).data[(*ctx).datalen] = data[i];
+              (*ctx).data[(*ctx).datalen] = text1[i];
               (*ctx).datalen ++;
               if ((*ctx).datalen == 64){
                   sha256_transform(ctx);
@@ -153,46 +153,52 @@ function App() {
           {
             var i : u32 = (*ctx).datalen;
           
-            // i = ctx->datalen;
-          
             // // Pad whatever data is left in the buffer.
-            // if (ctx->datalen < 56) {
-            //   ctx->data[i++] = 0x80;
-            //   while (i < 56)
-            //     ctx->data[i++] = 0x00;
-            // }
-            // else {
-            //   ctx->data[i++] = 0x80;
-            //   while (i < 64)
-            //     ctx->data[i++] = 0x00;
-            //   sha256_transform(ctx, ctx->data);
-            //   memset(ctx->data, 0, 56);
-            // }
+            if ((*ctx).datalen < 56) {
+              (*ctx).data[i] = 0x80;
+              i++;
+              while (i < 56) {
+                (*ctx).data[i] = 0x00;
+                i++;
+              }
+            }
+            else {
+              (*ctx).data[i] = 0x80;
+              i++;
+              while (i < 64) {
+                (*ctx).data[i] = 0x00;
+                i++;
+              }
+              sha256_transform(ctx);
+              for (var i = 0; i < 56 ; i++) {
+                (*ctx).data[i] = 0;
+              }
+            }
           
-            // // Append to the padding the total message's length in bits and transform.
-            // ctx->bitlen += ctx->datalen * 8;
-            // ctx->data[63] = ctx->bitlen;
-            // ctx->data[62] = ctx->bitlen >> 8;
-            // ctx->data[61] = ctx->bitlen >> 16;
-            // ctx->data[60] = ctx->bitlen >> 24;
-            // ctx->data[59] = ctx->bitlen >> 32;
-            // ctx->data[58] = ctx->bitlen >> 40;
-            // ctx->data[57] = ctx->bitlen >> 48;
-            // ctx->data[56] = ctx->bitlen >> 56;
-            // sha256_transform(ctx, ctx->data);
+            // Append to the padding the total message's length in bits and transform.
+            (*ctx).bitlen += (*ctx).datalen * 8;
+            (*ctx).data[63] = (*ctx).bitlen;
+            (*ctx).data[62] = (*ctx).bitlen >> 8;
+            (*ctx).data[61] = (*ctx).bitlen >> 16;
+            (*ctx).data[60] = (*ctx).bitlen >> 24;
+            (*ctx).data[59] = (*ctx).bitlen >> 32;
+            (*ctx).data[58] = (*ctx).bitlen >> 40;
+            (*ctx).data[57] = (*ctx).bitlen >> 48;
+            (*ctx).data[56] = (*ctx).bitlen >> 56;
+            sha256_transform(ctx);
           
-            // // Since this implementation uses little endian byte ordering and SHA uses big endian,
-            // // reverse all the bytes when copying the final state to the output hash.
-            // for (i = 0; i < 4; ++i) {
-            //   hash[i]      = (ctx->state[0] >> (24 - i * 8)) & 0x000000ff;
-            //   hash[i + 4]  = (ctx->state[1] >> (24 - i * 8)) & 0x000000ff;
-            //   hash[i + 8]  = (ctx->state[2] >> (24 - i * 8)) & 0x000000ff;
-            //   hash[i + 12] = (ctx->state[3] >> (24 - i * 8)) & 0x000000ff;
-            //   hash[i + 16] = (ctx->state[4] >> (24 - i * 8)) & 0x000000ff;
-            //   hash[i + 20] = (ctx->state[5] >> (24 - i * 8)) & 0x000000ff;
-            //   hash[i + 24] = (ctx->state[6] >> (24 - i * 8)) & 0x000000ff;
-            //   hash[i + 28] = (ctx->state[7] >> (24 - i * 8)) & 0x000000ff;
-            // }
+            // Since this implementation uses little endian byte ordering and SHA uses big endian,
+            // reverse all the bytes when copying the final state to the output hash.
+            for (var i : u32 = 0; i < 4; i++) {
+              (*hash)[i]      = ((*ctx).state[0] >> (24 - i * 8)) & 0x000000ff;
+              (*hash)[i + 4]  = ((*ctx).state[1] >> (24 - i * 8)) & 0x000000ff;
+              (*hash)[i + 8]  = ((*ctx).state[2] >> (24 - i * 8)) & 0x000000ff;
+              (*hash)[i + 12] = ((*ctx).state[3] >> (24 - i * 8)) & 0x000000ff;
+              (*hash)[i + 16] = ((*ctx).state[4] >> (24 - i * 8)) & 0x000000ff;
+              (*hash)[i + 20] = ((*ctx).state[5] >> (24 - i * 8)) & 0x000000ff;
+              (*hash)[i + 24] = ((*ctx).state[6] >> (24 - i * 8)) & 0x000000ff;
+              (*hash)[i + 28] = ((*ctx).state[7] >> (24 - i * 8)) & 0x000000ff;
+            }
           }
 
           @compute @workgroup_size(1, 1)
