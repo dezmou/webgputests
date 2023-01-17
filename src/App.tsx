129a64ebdddbb07621b53554829ebfd4c2fa788e
Dezmou <incredibledez@gmail.com>
2023-01-17 14:41:55 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -69,32 +69,54 @@ function App() {
 
       const shaderModule = device.createShaderModule({
         code: /*WGSL*/`
-        struct SHA256_CTX {
-          data : array<u32, 64>,
-          datalen : u32,
-          bitlen : u32,
-          state : array<u32, 8>,
-        };
-        
-        const WORD = array<u32, 64> (
-          0x428a2f98,0x71374491,0xb5c0fbcf,0xe9b5dba5,0x3956c25b,0x59f111f1,0x923f82a4,0xab1c5ed5,
-          0xd807aa98,0x12835b01,0x243185be,0x550c7dc3,0x72be5d74,0x80deb1fe,0x9bdc06a7,0xc19bf174,
-          0xe49b69c1,0xefbe4786,0x0fc19dc6,0x240ca1cc,0x2de92c6f,0x4a7484aa,0x5cb0a9dc,0x76f988da,
-          0x983e5152,0xa831c66d,0xb00327c8,0xbf597fc7,0xc6e00bf3,0xd5a79147,0x06ca6351,0x14292967,
-          0x27b70a85,0x2e1b2138,0x4d2c6dfc,0x53380d13,0x650a7354,0x766a0abb,0x81c2c92e,0x92722c85,
-          0xa2bfe8a1,0xa81a664b,0xc24b8b70,0xc76c51a3,0xd192e819,0xd6990624,0xf40e3585,0x106aa070,
-          0x19a4c116,0x1e376c08,0x2748774c,0x34b0bcb5,0x391c0cb3,0x4ed8aa4a,0x5b9cca4f,0x682e6ff3,
-          0x748f82ee,0x78a5636f,0x84c87814,0x8cc70208,0x90befffa,0xa4506ceb,0xbef9a3f7,0xc67178f2
-        );
+          struct SHA256_CTX {
+            data : array<u32, 64>,
+            datalen : u32,
+            bitlen : u32,
+            state : array<u32, 8>,
+          };
 
+          @group(0) @binding(0) var<storage, read> data : array<u32>;
+          @group(0) @binding(1) var<storage, read_write> result : array<u32>;
 
-          @group(0) @binding(0) var<storage, read> firstMatrix : array<i32>;
-          @group(0) @binding(1) var<storage, read_write> resultMatrix : array<i32>;
+          const LEN = 2;
+
+          const WORD = array<u32, 64> (
+            0x428a2f98,0x71374491,0xb5c0fbcf,0xe9b5dba5,0x3956c25b,0x59f111f1,0x923f82a4,0xab1c5ed5,
+            0xd807aa98,0x12835b01,0x243185be,0x550c7dc3,0x72be5d74,0x80deb1fe,0x9bdc06a7,0xc19bf174,
+            0xe49b69c1,0xefbe4786,0x0fc19dc6,0x240ca1cc,0x2de92c6f,0x4a7484aa,0x5cb0a9dc,0x76f988da,
+            0x983e5152,0xa831c66d,0xb00327c8,0xbf597fc7,0xc6e00bf3,0xd5a79147,0x06ca6351,0x14292967,
+            0x27b70a85,0x2e1b2138,0x4d2c6dfc,0x53380d13,0x650a7354,0x766a0abb,0x81c2c92e,0x92722c85,
+            0xa2bfe8a1,0xa81a664b,0xc24b8b70,0xc76c51a3,0xd192e819,0xd6990624,0xf40e3585,0x106aa070,
+            0x19a4c116,0x1e376c08,0x2748774c,0x34b0bcb5,0x391c0cb3,0x4ed8aa4a,0x5b9cca4f,0x682e6ff3,
+            0x748f82ee,0x78a5636f,0x84c87814,0x8cc70208,0x90befffa,0xa4506ceb,0xbef9a3f7,0xc67178f2
+          );
+
+          // fn sha256_update(ctx : SHA256_CTX , len : u32)
+          // {
+          //   // var i : u32 = 0;
+          //   for (var i: u32 = 0; i < len; i++){
+
+          //   }
+
+          //   // for (i = 0; i < len; ++i) {
+          //   //   ctx->data[ctx->datalen] = data[i];
+          //   //   ctx->datalen++;
+          //   //   if (ctx->datalen == 64) {
+          //   //     sha256_transform(ctx, ctx->data);
+          //   //     ctx->bitlen += 512;
+          //   //     ctx->datalen = 0;
+          //   //   }
+          //   // }
+          // }
+          
+
       
           @compute @workgroup_size(1, 1)
           fn main(@builtin(global_invocation_id) global_id : vec3<u32>) {
             var ctx : SHA256_CTX;
             
+            // CTX INIT
             ctx.datalen = 0;
             ctx.bitlen = 0;
             ctx.state[0] = 0x6a09e667;
@@ -106,8 +128,26 @@ function App() {
             ctx.state[6] = 0x1f83d9ab;
             ctx.state[7] = 0x5be0cd19;
 
+
+            // sha256_update
+            for (var i: u32 = 0; i < LEN; i++){
+              ctx.data[ctx.datalen] = data[i];
+              ctx.datalen ++;
+            }
+            // for (i = 0; i < len; ++i) {
+            //   ctx->data[ctx->datalen] = data[i];
+            //   ctx->datalen++;
+            //   if (ctx->datalen == 64) {
+            //     sha256_transform(ctx, ctx->data);
+            //     ctx->bitlen += 512;
+            //     ctx->datalen = 0;
+            //   }
+            // }
+
+
+
             let index = global_id.x;
-            resultMatrix[index] = firstMatrix[index] * 2;
+            result[index] = data[index] * 2;
           }
         `
       });
