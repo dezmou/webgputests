5caaaad3cbc873113cea763aff9170278182d808
Dezmou <incredibledez@gmail.com>
2023-01-17 15:47:50 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -74,10 +74,13 @@ function App() {
             datalen : u32,
             bitlen : u32,
             state : array<u32, 8>,
+            info : u32,
           };
 
           @group(0) @binding(0) var<storage, read> data : array<u32>;
           @group(0) @binding(1) var<storage, read_write> result : array<u32>;
+
+          const SHA256_BLOCK_SIZE = 32;
 
           const WORD = array<u32, 64> (
             0x428a2f98,0x71374491,0xb5c0fbcf,0xe9b5dba5,0x3956c25b,0x59f111f1,0x923f82a4,0xab1c5ed5,
@@ -146,10 +149,57 @@ function App() {
             }
           }
       
+          fn sha256_final(ctx : ptr<function, SHA256_CTX>, hash:  ptr<function, array<u32, SHA256_BLOCK_SIZE>>  )
+          {
+            var i : u32 = (*ctx).datalen;
+          
+            // i = ctx->datalen;
+          
+            // // Pad whatever data is left in the buffer.
+            // if (ctx->datalen < 56) {
+            //   ctx->data[i++] = 0x80;
+            //   while (i < 56)
+            //     ctx->data[i++] = 0x00;
+            // }
+            // else {
+            //   ctx->data[i++] = 0x80;
+            //   while (i < 64)
+            //     ctx->data[i++] = 0x00;
+            //   sha256_transform(ctx, ctx->data);
+            //   memset(ctx->data, 0, 56);
+            // }
+          
+            // // Append to the padding the total message's length in bits and transform.
+            // ctx->bitlen += ctx->datalen * 8;
+            // ctx->data[63] = ctx->bitlen;
+            // ctx->data[62] = ctx->bitlen >> 8;
+            // ctx->data[61] = ctx->bitlen >> 16;
+            // ctx->data[60] = ctx->bitlen >> 24;
+            // ctx->data[59] = ctx->bitlen >> 32;
+            // ctx->data[58] = ctx->bitlen >> 40;
+            // ctx->data[57] = ctx->bitlen >> 48;
+            // ctx->data[56] = ctx->bitlen >> 56;
+            // sha256_transform(ctx, ctx->data);
+          
+            // // Since this implementation uses little endian byte ordering and SHA uses big endian,
+            // // reverse all the bytes when copying the final state to the output hash.
+            // for (i = 0; i < 4; ++i) {
+            //   hash[i]      = (ctx->state[0] >> (24 - i * 8)) & 0x000000ff;
+            //   hash[i + 4]  = (ctx->state[1] >> (24 - i * 8)) & 0x000000ff;
+            //   hash[i + 8]  = (ctx->state[2] >> (24 - i * 8)) & 0x000000ff;
+            //   hash[i + 12] = (ctx->state[3] >> (24 - i * 8)) & 0x000000ff;
+            //   hash[i + 16] = (ctx->state[4] >> (24 - i * 8)) & 0x000000ff;
+            //   hash[i + 20] = (ctx->state[5] >> (24 - i * 8)) & 0x000000ff;
+            //   hash[i + 24] = (ctx->state[6] >> (24 - i * 8)) & 0x000000ff;
+            //   hash[i + 28] = (ctx->state[7] >> (24 - i * 8)) & 0x000000ff;
+            // }
+          }
+
           @compute @workgroup_size(1, 1)
           fn main(@builtin(global_invocation_id) global_id : vec3<u32>) {
             var ctx : SHA256_CTX;
-            
+            var buf : array<u32, SHA256_BLOCK_SIZE>;
+
             // CTX INIT
             ctx.datalen = 0;
             ctx.bitlen = 0;
@@ -165,9 +215,10 @@ function App() {
 
             var len : u32 = 2;
             sha256_update(&ctx, len);
+            sha256_final(&ctx, &buf);
 
             // let index = global_id.x;
-            result[0] = ctx.datalen;
+            result[0] = ctx.info;
           }
         `
       });
