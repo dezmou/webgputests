5c8b49c7a84bc3485a15ecbceded7559b4fb04eb
Dezmou <incredibledez@gmail.com>
2023-01-17 10:39:18 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -12,11 +12,7 @@ function App() {
 
       // First Matrix
 
-      const firstMatrix = new Float32Array([
-        2 /* rows */, 4 /* columns */,
-        1, 2, 3, 4,
-        5, 6, 7, 8
-      ]);
+      const firstMatrix = new Float32Array(Array.from({length : 257}).map((e,i) => i));
 
       const gpuBufferFirstMatrix = device.createBuffer({
         mappedAtCreation: true,
@@ -27,35 +23,12 @@ function App() {
       new Float32Array(arrayBufferFirstMatrix).set(firstMatrix);
       gpuBufferFirstMatrix.unmap();
 
-
-      // Second Matrix
-
-      const secondMatrix = new Float32Array([
-        4 /* rows */, 2 /* columns */,
-        1, 2,
-        3, 4,
-        5, 6,
-        7, 8
-      ]);
-
-      const gpuBufferSecondMatrix = device.createBuffer({
-        mappedAtCreation: true,
-        size: secondMatrix.byteLength,
-        usage: GPUBufferUsage.STORAGE,
-      });
-      const arrayBufferSecondMatrix = gpuBufferSecondMatrix.getMappedRange();
-      new Float32Array(arrayBufferSecondMatrix).set(secondMatrix);
-      gpuBufferSecondMatrix.unmap();
-
-
       // Result Matrix
-
-      const resultMatrixBufferSize = Float32Array.BYTES_PER_ELEMENT * (2 + firstMatrix[0] * secondMatrix[1]);
+      const resultMatrixBufferSize = firstMatrix.byteLength;
       const resultMatrixBuffer = device.createBuffer({
         size: resultMatrixBufferSize,
         usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC
       });
-
 
       const bindGroupLayout = (device as any).createBindGroupLayout({
         entries: [
@@ -68,13 +41,6 @@ function App() {
           },
           {
             binding: 1,
-            visibility: GPUShaderStage.COMPUTE,
-            buffer: {
-              type: "read-only-storage"
-            }
-          },
-          {
-            binding: 2,
             visibility: GPUShaderStage.COMPUTE,
             buffer: {
               type: "storage"
@@ -95,12 +61,6 @@ function App() {
           {
             binding: 1,
             resource: {
-              buffer: gpuBufferSecondMatrix
-            }
-          },
-          {
-            binding: 2,
-            resource: {
               buffer: resultMatrixBuffer
             }
           }
@@ -109,34 +69,14 @@ function App() {
 
       const shaderModule = device.createShaderModule({
         code: /*WGSL*/`
-          struct Matrix {
-            size : vec2<f32>,
-            numbers: array<f32>,
-          }
       
-          @group(0) @binding(0) var<storage, read> firstMatrix : Matrix;
-          @group(0) @binding(1) var<storage, read> secondMatrix : Matrix;
-          @group(0) @binding(2) var<storage, read_write> resultMatrix : Matrix;
+          @group(0) @binding(0) var<storage, read> firstMatrix : array<f32>;
+          @group(0) @binding(1) var<storage, read_write> resultMatrix : array<i32>;
       
-          @compute @workgroup_size(8, 8)
+          @compute @workgroup_size(256, 1)
           fn main(@builtin(global_invocation_id) global_id : vec3<u32>) {
-            // Guard against out-of-bounds work group sizes
-            if (global_id.x >= u32(firstMatrix.size.x) || global_id.y >= u32(secondMatrix.size.y)) {
-              return;
-            }
-      
-            resultMatrix.size = vec2(firstMatrix.size.x, secondMatrix.size.y);
-      
-            let resultCell = vec2(global_id.x, global_id.y);
-            var result = 0.0;
-            for (var i = 0u; i < u32(firstMatrix.size.y); i = i + 1u) {
-              let a = i + resultCell.x * u32(firstMatrix.size.y);
-              let b = resultCell.y + i * u32(secondMatrix.size.y);
-              result = result + firstMatrix.numbers[a] * secondMatrix.numbers[b];
-            }
-      
-            let index = resultCell.y + resultCell.x * u32(secondMatrix.size.y);
-            resultMatrix.numbers[index] = result;
+            let index = global_id.x;
+            resultMatrix[index] = 69;
           }
         `
       });
@@ -156,9 +96,8 @@ function App() {
       const passEncoder = commandEncoder.beginComputePass();
       passEncoder.setPipeline(computePipeline);
       passEncoder.setBindGroup(0, bindGroup);
-      const workgroupCountX = Math.ceil(firstMatrix[0] / 8);
-      const workgroupCountY = Math.ceil(secondMatrix[1] / 8);
-      passEncoder.dispatchWorkgroups(workgroupCountX, workgroupCountY);
+
+      passEncoder.dispatchWorkgroups(1, 1);
       passEncoder.end();
 
       // Get a GPU buffer for reading in an unmapped state.
@@ -182,7 +121,7 @@ function App() {
 
       await gpuReadBuffer.mapAsync(GPUMapMode.READ);
       const arrayBuffer = gpuReadBuffer.getMappedRange();
-      console.log(new Float32Array(arrayBuffer));
+      console.log(new Int32Array(arrayBuffer));
     })()
   }, [])
   return <>
