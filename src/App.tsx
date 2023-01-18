7437a8472cafd8beea8758b86458853360ac37b2
Dezmou <incredibledez@gmail.com>
2023-01-18 10:37:14 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -123,12 +123,12 @@ function App() {
             0x748f82ee,0x78a5636f,0x84c87814,0x8cc70208,0x90befffa,0xa4506ceb,0xbef9a3f7,0xc67178f2
           );
 
-          fn DBL_INT_ADD(a:u32,b:u32,c:u32) -> u32 {
-            if (a > 0xffffffff - (c)) {
-              return b + 1;
-            };
-            return a + c;
-          }
+          // fn DBL_INT_ADD(a:u32,b:u32,c:u32) -> u32 {
+          //   if (a > 0xffffffff - (c)) {
+          //     return b + 1;
+          //   };
+          //   return a + c;
+          // }
 
           fn ROTLEFT(a : u32, b : u32) -> u32{return (((a) << (b)) | ((a) >> (32-(b))));}
           fn ROTRIGHT(a : u32, b : u32) -> u32{return (((a) >> (b)) | ((a) << (32-(b))));}
@@ -226,7 +226,56 @@ function App() {
       
           fn sha256_final(ctx : ptr<function, SHA256_CTX>, hash:  ptr<function, array<u32, SHA256_BLOCK_SIZE>>  )
           {
+            var i : u32 = (*ctx).datalen;
 
+            if ((*ctx).datalen < 56) {
+              (*ctx).data[i] = 0x80;
+                i++;
+              while (i < 56){
+                (*ctx).data[i] = 0x00;
+                i++;
+              }
+            }
+            else {
+              (*ctx).data[i] = 0x80;
+              i++;
+              while (i < 64){
+                (*ctx).data[i] = 0x00;
+                i++;
+              }
+              sha256_transform(ctx);
+              for (var i = 0; i < 56 ; i++) {
+                (*ctx).data[i] = 0;
+              }
+            }
+          
+            // DBL_INT_ADD((*ctx).bitlen[0], (*ctx).bitlen[1], (*ctx).datalen * 8);
+            if ((*ctx).bitlen[0] > 0xffffffff - (*ctx).datalen * 8) {
+              (*ctx).bitlen[1]++;
+            }
+            (*ctx).bitlen[0] += (*ctx).datalen * 8;
+
+
+            (*ctx).data[63] = (*ctx).bitlen[0];
+            (*ctx).data[62] = (*ctx).bitlen[0] >> 8;
+            (*ctx).data[61] = (*ctx).bitlen[0] >> 16;
+            (*ctx).data[60] = (*ctx).bitlen[0] >> 24;
+            (*ctx).data[59] = (*ctx).bitlen[1];
+            (*ctx).data[58] = (*ctx).bitlen[1] >> 8;
+            (*ctx).data[57] = (*ctx).bitlen[1] >> 16;
+            (*ctx).data[56] = (*ctx).bitlen[1] >> 24;
+            sha256_transform(ctx);
+          
+            for (i = 0; i < 4; i++) {
+              (*hash)[i] = ((*ctx).state[0] >> (24 - i * 8)) & 0x000000ff;
+              (*hash)[i + 4] = ((*ctx).state[1] >> (24 - i * 8)) & 0x000000ff;
+              (*hash)[i + 8] = ((*ctx).state[2] >> (24 - i * 8)) & 0x000000ff;
+              (*hash)[i + 12] = ((*ctx).state[3] >> (24 - i * 8)) & 0x000000ff;
+              (*hash)[i + 16] = ((*ctx).state[4] >> (24 - i * 8)) & 0x000000ff;
+              (*hash)[i + 20] = ((*ctx).state[5] >> (24 - i * 8)) & 0x000000ff;
+              (*hash)[i + 24] = ((*ctx).state[6] >> (24 - i * 8)) & 0x000000ff;
+              (*hash)[i + 28] = ((*ctx).state[7] >> (24 - i * 8)) & 0x000000ff;
+            }
           }
 
           @compute @workgroup_size(1, 1)
